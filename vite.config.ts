999e5ee80a0af1281edb88a094ef6415fc412c2a
Jure Perak <jure.perak@moberg.hr>
2023-07-09 21:04:05 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/",
+    base: "/admin-portal/",
     plugins: [react(), svgr()],
     resolve: {
         alias: {
