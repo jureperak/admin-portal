0584500a3d200e0b46584ace85bdc48c367b7114
Jure Perak <jure.perak@moberg.hr>
2023-07-09 08:26:59 +0200

    preparing for build

--- src/pages/private/disputes/disputes.tsx
+++ src/pages/private/disputes/disputes.tsx
@@ -1,45 +1,45 @@
-import { ColumnDef } from "@tanstack/react-table";
-import { useMemo } from "react";
-import { DisputeModel } from "./dispute-models";
+// import { ColumnDef } from "@tanstack/react-table";
+// import { useMemo } from "react";
+// import { DisputeModel } from "./dispute-models";
 import { PrimarySolidButton } from "@/components";
 // import { InfiniteTable } from "@/features/table";
 
 export function Disputes() {
-    const columns = useMemo<ColumnDef<DisputeModel>[]>(
-        () => [
-            {
-                accessorFn: row => row.merchantName,
-                id: "merchantName",
-                cell: info => info.getValue(),
-                header: () => <span>Merchant</span>,
-            },
-            {
-                accessorFn: row => row.contractNumber,
-                id: "contractNumber",
-                cell: info => info.getValue(),
-                header: () => <span>Contract</span>,
-            },
-            {
-                accessorFn: row => row.registrationDate,
-                id: "registrationDate",
-                cell: info => info.getValue(),
-                header: () => <span>Registration Date</span>,
-            },
-            {
-                accessorFn: row => row.purchaseDate,
-                id: "purchaseDate",
-                cell: info => info.getValue(),
-                header: () => <span>Purchase Date</span>,
-            },
-            {
-                accessorFn: row => row.grossAmount,
-                id: "grossAmount",
-                cell: info => info.getValue(),
-                header: () => <span>Gross Amount</span>,
-            },
-        ],
-        []
-    );
+    // const columns = useMemo<ColumnDef<DisputeModel>[]>(
+    //     () => [
+    //         {
+    //             accessorFn: row => row.merchantName,
+    //             id: "merchantName",
+    //             cell: info => info.getValue(),
+    //             header: () => <span>Merchant</span>,
+    //         },
+    //         {
+    //             accessorFn: row => row.contractNumber,
+    //             id: "contractNumber",
+    //             cell: info => info.getValue(),
+    //             header: () => <span>Contract</span>,
+    //         },
+    //         {
+    //             accessorFn: row => row.registrationDate,
+    //             id: "registrationDate",
+    //             cell: info => info.getValue(),
+    //             header: () => <span>Registration Date</span>,
+    //         },
+    //         {
+    //             accessorFn: row => row.purchaseDate,
+    //             id: "purchaseDate",
+    //             cell: info => info.getValue(),
+    //             header: () => <span>Purchase Date</span>,
+    //         },
+    //         {
+    //             accessorFn: row => row.grossAmount,
+    //             id: "grossAmount",
+    //             cell: info => info.getValue(),
+    //             header: () => <span>Gross Amount</span>,
+    //         },
+    //     ],
+    //     []
+    // );
 
     return (
         <>
--- src/pages/private/merchants/merchants.tsx
+++ src/pages/private/merchants/merchants.tsx
@@ -1,49 +1,49 @@
-import { ColumnDef } from "@tanstack/react-table";
-import { useContext, useEffect, useMemo } from "react";
-import { MerchantModel } from "@/pages";
+// import { ColumnDef } from "@tanstack/react-table";
+import { useContext, useEffect } from "react";
+// import { MerchantModel } from "@/pages";
 // import { InfiniteTableQuery } from "@/features/table";
-import { useNavigate } from "react-router-dom";
-import { PATHS } from "@/lib";
+// import { useNavigate } from "react-router-dom";
+// import { PATHS } from "@/lib";
 import { MerchantContext } from "@/context";
 
 export function Merchants() {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     const { setSelectedMerchant } = useContext(MerchantContext);
 
     useEffect(() => {
         setSelectedMerchant({});
     }, []);
 
-    const columns = useMemo<ColumnDef<MerchantModel>[]>(
-        () => [
-            {
-                accessorFn: row => row.id,
-                id: "id",
-                cell: info => info.getValue(),
-                header: () => <span>Id</span>,
-            },
-            {
-                accessorFn: row => row.name,
-                id: "name",
-                cell: info => info.getValue(),
-                header: () => <span>Name</span>,
-            },
-            {
-                accessorFn: row => row.ssn,
-                id: "ssn",
-                cell: info => info.getValue(),
-                header: () => <span>SSN</span>,
-            },
+    // const columns = useMemo<ColumnDef<MerchantModel>[]>(
+    //     () => [
+    //         {
+    //             accessorFn: row => row.id,
+    //             id: "id",
+    //             cell: info => info.getValue(),
+    //             header: () => <span>Id</span>,
+    //         },
+    //         {
+    //             accessorFn: row => row.name,
+    //             id: "name",
+    //             cell: info => info.getValue(),
+    //             header: () => <span>Name</span>,
+    //         },
+    //         {
+    //             accessorFn: row => row.ssn,
+    //             id: "ssn",
+    //             cell: info => info.getValue(),
+    //             header: () => <span>SSN</span>,
+    //         },
 
-            {
-                accessorFn: row => row.createdTime,
-                id: "createdTime",
-                cell: info => info.getValue(),
-                header: () => <span>Created</span>,
-            },
-        ],
-        []
-    );
+    //         {
+    //             accessorFn: row => row.createdTime,
+    //             id: "createdTime",
+    //             cell: info => info.getValue(),
+    //             header: () => <span>Created</span>,
+    //         },
+    //     ],
+    //     []
+    // );
 
     return (
         <>
